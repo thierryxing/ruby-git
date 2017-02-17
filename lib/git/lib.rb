2bed57fdbbe1860936616a76404dd2454b3173fd
Thierry <thierry.xing@gmail.com>
2017-02-17 14:42:58 +0800

    fix git log message character issue

--- lib/git/lib.rb
+++ lib/git/lib.rb
@@ -859,10 +859,6 @@ module Git
 
     def command_lines(cmd, opts = [], chdir = true, redirect = '')
       cmd_op = command(cmd, opts, chdir)
-      op = cmd_op.encode("UTF-8", "binary", {
-	  	:invalid => :replace,
-		:undef => :replace
-	  })
       op.split("\n")
     end
 
